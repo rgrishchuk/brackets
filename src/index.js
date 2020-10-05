dea90b923ff6876dfbfa6e2a0700117e111ef6dc
Ruslan Hryshchuk <rus_g@tut.by>
2020-10-05 21:48:44 +0300

    feat: solved brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let brackets = {};
+	bracketsConfig.forEach((item) =>{ 
+		brackets[item[1]] = item[0];
+	});
+	resArray = [];
+	for (let i = 0; i < str.length; i++) {
+		if (i == 0) resArray.push(str[0])
+		else {
+			if (resArray.length !=0 && resArray[resArray.length - 1] == brackets[str[i]]) {
+				resArray.pop();
+			} else {
+				resArray.push(str[i]);
+			}	
+		}	
+	}
+	return resArray.length == 0;
 }
